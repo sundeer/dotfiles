d8f1760849d006a6581cc7a3a07971f383ebcacb
sundeer <sundeer@rhsjmm.com>
2025-04-09 17:19:52 -0400

    Update .config/homebrew/chezmoi-brewfile-dump.sh

--- dot_config/private_homebrew/executable_chezmoi-brewfile-dump.sh
+++ dot_config/private_homebrew/executable_chezmoi-brewfile-dump.sh
@@ -4,8 +4,12 @@ timestamp() {
     date '+%Y-%m-%d %H:%M:%S'
 }
 
+Log() {
+    echo "[$(timestamp)] LOG: $1" >&1
+}
+
 exitOnError() {
-    echo "$(timestamp) ERROR: $1" >&2
+    echo "$(timestamp) ERROR: $1" >&1
     exit 1
 }
 
@@ -13,53 +17,53 @@ exitOnError() {
 # It adds error handling for each step, reporting to stdout and stderr.
 
 # Step 1: Dump Brewfile using brew bundle dump
-echo "$(timestamp) Dumping Brewfile..."
+Log "Dumping Brewfile..."
 if /opt/homebrew/bin/brew bundle dump --force --file="${HOME}/.local/share/chezmoi/dot_config/Brewfile"; then
-    echo "$(timestamp) Brewfile dumped successfully."
+    Log "Brewfile dumped successfully."
 else
     exitOnError "Failed to dump Brewfile."
 fi
 
 # Step 2: Stage the Brewfile changes with chezmoi
-echo "$(timestamp) Adding Brewfile to chezmoi git..."
+Log "Adding Brewfile to chezmoi git..."
 if chezmoi git add ./dot_config/Brewfile; then
-    echo "$(timestamp) Brewfile added successfully."
+    Log "Brewfile added successfully."
 else
     exitOnError "Failed to add Brewfile with chezmoi git."
 fi
 
 # Step 3: Commit the changes
-echo "$(timestamp) Committing changes..."
+Log "Committing changes..."
 if chezmoi git -- commit -m "Modified: Brewfile"; then
-    echo "$(timestamp) Changes committed successfully."
+    Log "Changes committed successfully."
 else
     exitOnError "Failed to commit changes."
 fi
 
 # Step 4: Apply the changes using chezmoi
-echo "$(timestamp) Applying changes using chezmoi..."
+Log "Applying changes using chezmoi..."
 if chezmoi apply; then
-    echo "$(timestamp) Changes applied successfully."
+    Log "Changes applied successfully."
 else
     exitOnError "Failed to apply changes with chezmoi."
 fi
 
 # Step 5: Push the changes to the remote repository
-echo "$(timestamp) Pushing changes to remote repository..."
+Log "Pushing changes to remote repository..."
 if chezmoi git push; then
-    echo "$(timestamp) Changes pushed successfully."
+    Log "Changes pushed successfully."
 else
     exitOnError "Failed to push changes with chezmoi git."
 fi
 
 # Step 6: Apply the changes to the target directory
-echo "$(timestamp) Applying changes to target directory..."
+Log "Applying changes to target directory..."
 if chezmoi apply; then
-    echo "$(timestamp) Changes applied successfully."
+    Log "Changes applied successfully."
 else
     exitOnError "Failed to apply changes to target."
 fi
 
-echo "$(timestamp) ...ChezMoi Brewfile dump and apply completed successfully."
+Log "...ChezMoi Brewfile dump and apply completed successfully."
 
 exit 0
