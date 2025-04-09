d46d314f18392c6d9af4051196ebae428d135ecd
sundeer <sundeer@rhsjmm.com>
2025-04-09 18:24:06 -0400

    Update .config/homebrew/chezmoi-brewfile-dump.sh Update Library/LaunchAgents/com.rhsjmm.chezmoi.brewfile.plist

--- dot_config/private_homebrew/executable_chezmoi-brewfile-dump.sh
+++ dot_config/private_homebrew/executable_chezmoi-brewfile-dump.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
+exec 1>&2
 
 timestamp() {
     date '+%Y-%m-%d %H:%M:%S'
 }
 
 Log() {
-    echo "[$(timestamp)] LOG: $1" >&2
+    echo "[$(timestamp)] LOG: $1"
 }
 
 exitOnError() {
-    echo "[$(timestamp)] ERROR: $1" >&2
+    echo "[$(timestamp)] ERROR: $1"
     exit 1
 }
 
 # This script dumps the Brewfile and updates the chezmoi repository.
-# It adds error handling for each step, reporting to stdout and stderr.
+# It adds error handling for each step, reporting exclusively to stderr.
 
-echo "" >&2
+echo ""
 Log "Starting ChezMoi Brewfile dump and apply process"
 
 # Dump Brewfile using brew bundle dump (swallow stdout)
 Log "Dumping Brewfile..."
-if brew bundle dump --force --file="${HOME}/.local/share/chezmoi/dot_config/Brewfile" > /dev/null; then
+if brew bundle dump --force --file="${HOME}/.local/share/chezmoi/dot_config/Brewfile"; then
     Log "Brewfile dumped successfully."
 else
     exitOnError "Failed to dump Brewfile."
@@ -29,7 +30,7 @@ fi
 
 # Stage the Brewfile changes with chezmoi (swallow stdout)
 Log "Adding Brewfile to chezmoi git..."
-if chezmoi git add ./dot_config/Brewfile > /dev/null; then
+if chezmoi git add ./dot_config/Brewfile; then
     Log "Brewfile added successfully."
 else
     exitOnError "Failed to add Brewfile with chezmoi git."
@@ -37,7 +38,7 @@ fi
 
 # Commit the changes (swallow stdout)
 Log "Committing changes..."
-if chezmoi git -- commit -m "Modified: Brewfile" > /dev/null; then
+if chezmoi git -- commit -m "Modified: Brewfile"; then
     Log "Changes committed successfully."
 else
     exitOnError "Failed to commit changes."
@@ -45,7 +46,7 @@ fi
 
 # Push the changes to the remote repository (swallow stdout)
 Log "Pushing changes to remote repository..."
-if chezmoi git push > /dev/null; then
+if chezmoi git push; then
     Log "Changes pushed successfully."
 else
     exitOnError "Failed to push changes with chezmoi git."
@@ -53,7 +54,7 @@ fi
 
 # Apply the changes to the target directory (swallow stdout)
 Log "Applying changes to target directory..."
-if chezmoi apply > /dev/null; then
+if chezmoi apply; then
     Log "Changes applied successfully."
 else
     exitOnError "Failed to apply changes to target."
