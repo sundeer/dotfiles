f16ee783140fbfc806956549341f9b044e42b5b1
sundeer <sundeer@rhsjmm.com>
2025-04-11 15:18:26 -0400

    Update load-brewfile-launchagent.sh

--- run_once_after_load-brewfile-launchagent.sh
+++ run_once_after_load-brewfile-launchagent.sh
@@ -1,4 +1,4 @@
 # Load the job into launchd
 # launchctl load ~/Library/LaunchAgents/com.rhsjmm.chezmoi.brewfile.plist
 
-brew bundle --file="$HOME/.local/share/chezmoi/brewfile" --global
+/opt/homebre/bin/brew bundle --file="$HOME/.local/share/chezmoi/brewfile" --global
