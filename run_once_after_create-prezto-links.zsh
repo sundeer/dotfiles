8c950bd845d30482ee162c3948af0a18706ad46d
sundeer <sundeer@rhsjmm.com>
2025-04-12 16:01:22 -0400

    Remove create-prezto-links.sh Add create-prezto-links.zsh

---
+++ run_once_after_create-prezto-links.zsh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+
+# This script creates symbolic links for the Prezto runcoms.
+
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
