a6d0ae950efc408e13d5d649a9885440924306cd
sundeer <sundeer@rhsjmm.com>
2025-04-12 13:37:23 -0400

    Update load-brewfile-launchagent.sh Remove homebrew-install.sh Change attributes of .github/workflows/greenfield-install.yml

--- run_once_after_load-brewfile-launchagent.sh
+++ run_once_after_load-brewfile-launchagent.sh
@@ -15,19 +15,19 @@ exitOnError() {
     exit 1
 }
 
-# ---------------------------
-# Launchd job loading
-# ---------------------------
-Log "Loading launchd job..."
-if launchctl load ~/Library/LaunchAgents/com.rhsjmm.chezmoi.brewfile.plist; then
-    Log "Launchd job loaded successfully."
+# Install Homebrew
+Log "Installing Homebrew..."
+if command -v brew >/dev/null 2>&1; then
+    Log "Homebrew is already installed."
 else
-    exitOnError "Failed to load launchd job: ~/Library/LaunchAgents/com.rhsjmm.chezmoi.brewfile.plist"
+    if /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"; then
+        exitOnError "Failed to install Homebrew."
+    fi
+    Log "Homebrew installed successfully."
 fi
 
-# ----------------------------------
-# Homebrew operations
-# ----------------------------------
+# Install Homebrew dependencies
+Log "Checking for Homebrew dependencies..."
 if [ "$CI" = "true" ]; then
     # In CI mode, do not install packages, just list Homebrew dependencies.
     Log "CI environment detected, listing Homebrew dependencies..."
@@ -44,4 +44,24 @@ else
     else
         exitOnError "Failed to install Homebrew dependencies from Brewfile."
     fi
+fi
+
+# Load Homebrew launchd job
+Log "Checking for existing launchd job..."
+if launchctl list | grep -q "com.rhsjmm.chezmoi.brewfile"; then
+    Log "Launchd job already exists."
+else
+    Log "Creating launchd job..."
+    if cp ~/Library/LaunchAgents/com.rhsjmm.chezmoi.brewfile.plist ~/Library/LaunchAgents/; then
+        Log "Launchd job created successfully."
+    else
+        exitOnError "Failed to copy launchd job plist file."
+    fi
+fi
+
+Log "Loading launchd job..."
+if launchctl load ~/Library/LaunchAgents/com.rhsjmm.chezmoi.brewfile.plist; then
+    Log "Launchd job loaded successfully."
+else
+    exitOnError "Failed to load launchd job: ~/Library/LaunchAgents/com.rhsjmm.chezmoi.brewfile.plist"
 fi
