7f1ca6b749e2f244d2926d6f951c43e6fe8a1637
sundeer <sundeer@rhsjmm.com>
2025-04-15 12:01:23 -0400

    Update install-prezto.zsh

--- run_once_before_install-prezto.zsh
+++ run_once_before_install-prezto.zsh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 # This script installs prezto into ~/.config/zsh/.zprezto if it is not already installed.
 
-if [ ! -d "${$HOME}/.config/zsh/.zprezto" ]; then
+if [ ! -d "$HOME/.config/zsh/.zprezto" ]; then
   echo "Cloning prezto..."
-  git clone --recursive https://github.com/sorin-ionescu/prezto.git "${$HOME}/.config/zsh/.zprezto"
+  git clone --recursive https://github.com/sorin-ionescu/prezto.git "$HOME/.config/zsh/.zprezto"
 fi
