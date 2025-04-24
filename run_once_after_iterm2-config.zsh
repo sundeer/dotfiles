669ce41411156fb97139c4eb921bbf5496dc7590
sundeer <sundeer@rhsjmm.com>
2025-04-24 13:28:01 -0400

    Update .chezmoiignore Add iterm2-config.zsh

---
+++ run_once_after_iterm2-config.zsh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+# Specify the preferences directory
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.config/iterm2"
+
+# Tell iTerm2 to use the custom preferences in the directory
+defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
